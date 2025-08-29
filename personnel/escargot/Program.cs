d7d0dd772b49f5797d1373ee18397f0a9f11ae09
Damien-16 <pv77ngi@eduvaud.ch>
2025-08-29 11:10:31 +0200

    Ex Snail sans OO

---
+++ personnel/escargot/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace escargot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string snake = "_@_ö";
+            int PositionActu = 2;
+            Console.CursorVisible = false;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(PositionActu, 10);
+                Console.WriteLine(snake);
+                Console.SetCursorPosition(PositionActu - 1, 10);
+                Console.WriteLine(" ");
+                Console.SetCursorPosition(PositionActu, 10);
+                PositionActu++;
+                Thread.Sleep(50);
+            } while (PositionActu < 30);
+            Console.WriteLine("____");
+
+
+
+        }
+    }
+}
