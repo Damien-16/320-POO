69260e54c6a75d161bdf53096f4adbee132573a5
Damien-16 <pv77ngi@eduvaud.ch>
2025-09-05 09:28:44 +0200

    Ajouter les classe pour avion et para
    Ce commit ajoute au programme la classe de l'avion et des parachutistes

---
+++ personnel/Parachutiste/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using System.Numerics;
+
+namespace Parachutiste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+        static class Config
+        {
+            public const int SCREEN_HEIGHT = 40;
+            public const int SCREEN_WIDTH = 150;
+        }
+        class Plane
+        {
+            private string[] view =
+      {
+            @" _                         ",
+            @"| \                        ",
+            @"|  \       ______          ",
+            @"--- \_____/  |_|_\____  |  ",
+            @"  \_______ --------- __>-} ",
+            @"        \_____|_____/   |  "
+        };
+
+        
+        }
+    
+
+        class Para
+        {
+            private string[] withoutParachute =
+        {
+         @"     ",
+         @"     ",
+         @"     ",
+         @"  o  ",
+         @" /░\ ",
+         @" / \ ",
+        };
+            private string[] withParachute =
+            {
+         @" ___ ",
+         @"/|||\",
+         @"\   /",
+         @" \o/ ",
+         @"  ░  ",
+         @" / \ ",
+        };
+
+        }
+
+
+    }
+}
+
+    
+
